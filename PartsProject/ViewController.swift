8fe3b14b5f2d0e866f2adcd22844519d88546b10
Yuta Fujii <yutayutakid21@gmail.com>
2020-02-20 18:11:27 +0900

    課題

--- PartsProject/ViewController.swift
+++ PartsProject/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    /*
+     課題:ボタンA、Bを作成し、テキストフィールドを2つ置き、ラベルを置き、Aをタップするとテキストフィールドに入力されている値同士を足し算、Bをタップすると引き算ができるようにしてください。
+     また、条件としては、計算自体はモデルクラスを作成し、
+     ボタン、ラベルは独自クラスで作成してください。
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
