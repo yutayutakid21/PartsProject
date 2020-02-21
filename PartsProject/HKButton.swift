d93b3df4bffe95c612b7a89674c723f7efd18d9e
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-21 18:51:25 +0900

    CalcModelを作成

--- PartsProject/HKButton.swift
+++ PartsProject/HKButton.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+/*
+課題:ボタンA、Bを作成し、テキストフィールドを2つ置き、ラベルを置き、Aをタップするとテキストフィールドに入力されている値同士を足し算、Bをタップすると引き算ができるようにしてください。
+また、条件としては、計算自体はモデルクラスを作成し、
+ボタン、ラベルは独自クラスで作成してください。
+*/
+
 class HKButton: UIButton {
 
     override init(frame: CGRect) {
@@ -28,7 +34,7 @@ class HKButton: UIButton {
         configure()
     }
     
-     //GFButtonを作成するためのプロパティを定義
+     //HKButtonを作成するためのプロパティを定義
     private func configure(){
         layer.cornerRadius      = 10
         setTitleColor(.white, for: .normal)
