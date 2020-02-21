d8c625548a19d5c4ea252fcb183964ae48c8bde7
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-21 18:44:46 +0900

    TextFieldのプレースホルダーの内容を修正

---
+++ PartsProject/HKButton.swift
@@ -0,0 +1,44 @@
+//
+//  HKButton.swift
+//  PartsProject
+//
+//  Created by 近藤宏輝 on 2020/02/21.
+//  Copyright © 2020 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class HKButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(backgroundColor: UIColor, title: String){
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+     //GFButtonを作成するためのプロパティを定義
+    private func configure(){
+        layer.cornerRadius      = 10
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+    func set(backgroundColor: UIColor, title: String) {
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+    }
+}
